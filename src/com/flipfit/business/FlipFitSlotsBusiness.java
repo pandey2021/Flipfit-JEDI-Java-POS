1e3cc217864de05a37230e1b8518f52721d85d8c
Ritika-27 <chaudharyritika176@gmail.com>
2024-11-07 22:23:59 +0530

    added dao dependencies in flipfitslotsbusiness

--- src/com/flipfit/business/FlipFitSlotsBusiness.java
+++ src/com/flipfit/business/FlipFitSlotsBusiness.java
@@ -1,17 +1,21 @@
 package com.flipfit.business;
 import com.flipfit.bean.FlipFitSlots;
 import com.flipfit.business.interfaces.IFlipFitSlots;
+import com.flipfit.dao.classes.FlipFitSlotDAOImpl;
 
 
 public class FlipFitSlotsBusiness implements IFlipFitSlots {
 
     public boolean updateAvailability(FlipFitSlots flipFitSlots) {
         System.out.println("Updating Slot Availability");
+        FlipFitSlotDAOImpl flipFitSlotDAO = new FlipFitSlotDAOImpl();
+        flipFitSlotDAO.changeSlot(flipFitSlots);
         return true;
     }
 
     public void getSlotDetails() {
         System.out.println("Getting Slot Details");
-        return;
+        FlipFitSlotDAOImpl flipFitSlotDAO = new FlipFitSlotDAOImpl();
+
     }
 }
